d91db144af822ab29a1478a75e5050ca08f9027b
Adam Mills <adam.millsy@gmail.com>
2018-02-28 20:42:39 -0500

    caf_rotary_wing #1

---
+++ addons/caf_rotary_wing/config.cpp
@@ -0,0 +1,16 @@
+﻿
+
+
+#define _ARMA_
+
+//Class F:/A3Mods/@cesp2/addons/cesp/config.bin{
+class CfgPatches
+{
+	class CAF_Rot_m
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+};
