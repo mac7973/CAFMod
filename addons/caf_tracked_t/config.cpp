b54da15f7f739b960aab54889d96166a402e7dcd
Adam Mills <adam.millsy@gmail.com>
2018-02-28 22:50:40 -0500

    caf_tracked_t

---
+++ addons/caf_tracked_t/config.cpp
@@ -0,0 +1,11 @@
+class DefaultEventHandlers;
+class CfgPatches
+{
+	class caf_tracked_t
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Armor_F"};
+	};
+};
