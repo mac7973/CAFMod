e5904b32779fcbfb7bade9349045c3d22bd08e31
Adam Mills <adam.millsy@gmail.com>
2018-03-01 20:49:51 -0500

    caf_weapons_s

---
+++ addons/caf_weapons_s/config.cpp
@@ -0,0 +1,11 @@
+﻿class CfgPatches
+{
+	class CAF_WEAPONS
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Data_F","A3_Weapons_F"};
+		author = "Theebu";
+	};
+};
