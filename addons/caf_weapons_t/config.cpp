727ea7860a4ce0014f84994bed8e6e14f49a7f68
Adam Mills <adam.millsy@gmail.com>
2018-03-01 20:56:35 -0500

    caf_weapons_t #1

---
+++ addons/caf_weapons_t/config.cpp
@@ -0,0 +1,17 @@
+﻿
+
+
+#define _ARMA_
+
+//Class F:/A3Mods/@cesp2/addons/cesp/config.bin{
+class CfgPatches
+{
+	class CESP_WEAPONS
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Data_F","A3_Weapons_F","A3_Soft_F"};
+		author = "CEFCOM G4 BRANCH";
+	};
+};
